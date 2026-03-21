d2fa75e9680474b99f27d5940183ee6af1fe8de9
SukhamGeek <sukhamsingh3105@gmail.com>
2026-03-21 15:58:42 +0530

    Time: 55 ms (7.21%), Space: 9 MB (50.41%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        string s="a";
+        if(n==1&& k<4){
+          s[0]+=k-1;
+          return s;  
+        }
+        int a=0;
+        string ans=helper(n, k, "a", 1, a);
+        if(a==k&&ans!="") return ans;
+        ans=helper(n, k, "b", 1, a);
+        if(a==k&&ans!="") return ans;
+        ans=helper(n, k, "c", 1, a);
+        if(a==k) return ans;
+        return "";
+    }
+    string helper(int n, int k, string s, int size, int& o){
+        if(size==n){
+            o++;
+            cout<<s<<" "<<o<<endl;
+            return s;
+        }
+        if(s[size-1]=='a'){
+            string s1= helper(n, k, s+'b', size+1, o);
+            if(o==k) return s1;
+            string s2= helper(n, k, s+'c', size+1, o);
+            if(o==k) return s2;
+        }
+        else if(s[size-1]=='b'){
+            string s1= helper(n, k, s+'a', size+1, o);
+            if(o==k) return s1;
+            string s2= helper(n, k, s+'c', size+1, o);
+            if(o==k) return s2;
+        }
+        else{
+            string s1= helper(n, k, s+'a', size+1, o);
+            if(o==k) return s1;
+            string s2= helper(n, k, s+'b', size+1, o);
+            if(o==k) return s2;
+        }
+        return "";
+    }
+};
