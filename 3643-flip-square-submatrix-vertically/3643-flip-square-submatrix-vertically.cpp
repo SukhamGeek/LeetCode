9028ae300b931720c67e70fa3e6ee1508e802ff3
SukhamGeek <sukhamsingh3105@gmail.com>
2026-03-21 15:20:46 +0530

    Time: 0 ms (100%), Space: 33.8 MB (93.98%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
@@ -7,7 +7,7 @@ public:
         int p=0;
         for(int i=x; i<x+(k/2); i++){
             for(int j=y; j<(y+k); j++){
-                cout<<grid[i+k-1-p][j]<<endl;
+                
                 swap(grid[i][j],grid[i+k-1-2*p][j]);
             }
             p++;
