127cbe59b80c7ae1c9c11a06060c61793564bebf
SukhamGeek <sukhamsingh3105@gmail.com>
2026-04-06 13:24:26 +0530

    Time: 22 ms (68.8%), Space: 37.9 MB (49.07%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,70 +1,77 @@
 class Solution {
 public:
-    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        int x=0;
-        int y=0;
-        int p=3;
-        int ans= INT_MIN;
-        struct hash_pair {
-            size_t operator()(const pair<int,int>& p) const {
-                return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-            }
-        };
-        unordered_map<pair<int,int>, int, hash_pair> mpp;
-        for(int i=0; i<obstacles.size(); i++){
-            mpp[{obstacles[i][0],obstacles[i][1]}]++;
+    struct hash_pair {
+        size_t operator()(const pair<int,int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
         }
-        for(int i=0; i<commands.size(); i++){
-            if(commands[i]==-2){
-                p= (p+3)%4;
-                continue;
+    };
+
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        int x = 0;
+        int y = 0;
+        int p = 3;   // 0 east, 1 south, 2 west, 3 north
+        int ans = 0;
+
+        unordered_set<pair<int,int>, hash_pair> st;
+
+        for (int i = 0; i < obstacles.size(); i++) {
+            st.insert({obstacles[i][0], obstacles[i][1]});
+        }
+
+        for (int i = 0; i < commands.size(); i++) {
+
+            if (commands[i] == -2) {
+                p = (p + 3) % 4;
             }
-            else if(commands[i]==-1){
-                p= (p+1)%4;
-                continue;
+            else if (commands[i] == -1) {
+                p = (p + 1) % 4;
             }
-            else{
-                if(p==0){
-                    for(int j=0; j<commands[i]; j++){
+            else {
+
+                if (p == 0) {
+                    for (int j = 0; j < commands[i]; j++) {
                         x++;
-                        if(mpp[{x,y}]>0){
+                        if (st.find({x, y}) != st.end()) {
                             x--;
                             break;
                         }
                     }
                 }
-                if(p==1){
-                    for(int j=0; j<commands[i]; j++){
+
+                if (p == 1) {
+                    for (int j = 0; j < commands[i]; j++) {
                         y--;
-                        if(mpp[{x,y}]>0){
+                        if (st.find({x, y}) != st.end()) {
                             y++;
                             break;
                         }
                     }
                 }
-                if(p==2){
-                    for(int j=0; j<commands[i]; j++){
+
+                if (p == 2) {
+                    for (int j = 0; j < commands[i]; j++) {
                         x--;
-                        if(mpp[{x,y}]>0){
+                        if (st.find({x, y}) != st.end()) {
                             x++;
                             break;
                         }
                     }
                 }
-                if(p==3){
-                    for(int j=0; j<commands[i]; j++){
+
+                if (p == 3) {
+                    for (int j = 0; j < commands[i]; j++) {
                         y++;
-                        if(mpp[{x,y}]>0){
+                        if (st.find({x, y}) != st.end()) {
                             y--;
                             break;
                         }
                     }
                 }
-                
-                int temp= (x*x)+(y*y);
-                if(temp>ans) ans=temp;
+
+                ans = max(ans, x * x + y * y);
             }
         }
+
         return ans;
     }
 };
