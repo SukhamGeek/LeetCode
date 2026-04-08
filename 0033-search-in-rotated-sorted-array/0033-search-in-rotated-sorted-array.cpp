e75ad338324b0ce87600d6d4d3a42f5d3c7a7708
SukhamGeek <sukhamsingh3105@gmail.com>
2026-04-08 15:58:06 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,51 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int mid= rot(nums);
-        cout<<mid;
-        if(mid==-1){
-            mid= nums.size()/2;
-        }
-        int l1=0;
-        int h1=mid-1;
-        int l2= mid;
-        int h2= nums.size()-1;
-        int m1= l1+(h1-l1)/2;
-        int m2= l2+(h2-l2)/2;
-        while(l1<=h1){
-            if(nums[m1]==target) return m1;
-            else if(nums[m1]<target) l1= m1+1;
-            else h1= m1-1;
-            m1= l1+(h1-l1)/2;
-        }
-        while(l2<=h2){
-            if(nums[m2]==target) return m2;
-            else if(nums[m2]<target) l2= m2+1;
-            else h2= m2-1;
-            m2= l2+(h2-l2)/2;
+        int l=0;
+        int h= nums.size()-1;
+        int mid= l+(h-l)/2;
+        while(l<=h){
+            if(nums[mid]==target) return mid;
+            if(nums[l]<=nums[mid]){//left side is sorted check
+                if(target>=nums[l]&&target<nums[mid]) h=mid-1;
+                else l=mid+1;
+            }
+            else{//If left not sorted we move to the right side for the check
+                if(target>nums[mid]&&target<=nums[h]) l=mid+1;
+                else h=mid-1;
+            }
+            mid= l+(h-l)/2;
         }
         return -1;
+        //We are sure that atleast one of the left or right side is sorted in a scenario so we use it
     }
-    int rot(vector<int>& nums) {
-    int l = 0;
-    int h = nums.size() - 1;
-
-    // already sorted, no rotation
-    if(nums[l] <= nums[h]) return 0;
-
-    while(l <= h) {
-        int mid = l + (h - l) / 2;
-
-        // pivot found
-        if(mid < nums.size()-1 && nums[mid] > nums[mid+1])
-            return mid + 1;
-
-        if(nums[mid] >= nums[l])
-            l = mid + 1;
-        else
-            h = mid - 1;
-    }
-
-    return 0;
-}
 };
