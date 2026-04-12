d3fa9f6d3ffd62719a197bc9ef0d7dad0d484cbc
SukhamGeek <sukhamsingh3105@gmail.com>
2026-04-12 11:14:44 +0530

    Time: 60 ms (15.49%), Space: 159.6 MB (11.21%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        vector<int> arr;
-        const int mod= 12345;
-        int m= grid.size();
-        int n= grid[0].size();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                arr.push_back(grid[i][j]%12345);
+        const int mod = 12345;
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<int>> suff(m, vector<int>(n));
+        int prod = 1;
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                suff[i][j] = prod;
+                prod = (1LL * prod * grid[i][j]) % mod;
             }
         }
+
+        prod = 1;
+
         
-        vector<int> suff(arr.size());
-        vector<int> pre(arr.size());
-        suff[0]=1;
-        pre[arr.size()-1]=1;
-        for(int i=1; i<arr.size(); i++){
-            suff[i]= (suff[i-1]*arr[i-1])%mod;
-        }
-        for(int i= arr.size()-2; i>=0; i--){
-            pre[i]= (pre[i+1]*arr[i+1])%mod;
-        }
-        int k=0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                grid[i][j]= (suff[k]*pre[k])%mod;
-                k++;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int cur = grid[i][j];
+                grid[i][j] = (1LL * prod * suff[i][j]) % mod;
+                prod = (1LL * prod * cur) % mod;
             }
         }
+
         return grid;
     }
 };
