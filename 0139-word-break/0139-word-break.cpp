d936d7b145b9fb148a4dd4b9163b44ec2a383e03
SukhamGeek <sukhamsingh3105@gmail.com>
2026-06-16 01:41:27 +0530

    Time: 0 ms (100%), Space: 10.6 MB (86.2%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<vector<int>>dp(s.size(),vector<int>(s.size(),-1));
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
         int n= s.size();
-        return helper(s, 0, st, n, 0, "", dp);
-    }
-    bool helper(string& s, int ind, unordered_set<string>& st, int& n, int count, string temp,vector<vector<int>>&dp){
-        if(ind==n&&st.count(temp)){
-            return true;
+        vector<bool>vec(n+1,false);
+        vec[0]=true;
+        int maxi=INT_MIN;
+        unordered_map<string,int>mpp;
+        for(int i=0; i<wordDict.size(); i++){
+            maxi= max(maxi,(int)wordDict[i].size());
+            mpp[wordDict[i]]++;
         }
-        if(ind>=n) return false;
-        if(dp[ind][count]!=-1){
-            if(dp[ind][count]==1) return true;
-            return false;
+        for(int i=0; i<n; i++){
+            int j=i;
+            int temp=maxi;
+            while(j>=0&&temp>0){
+                if(mpp.find(s.substr(j,i-j+1))!=mpp.end()){
+                    if(j==0){
+                        cout<<i<<endl;
+                        vec[i+1]=true;
+                        break;
+                    } 
+                    else{
+                        if(vec[j]==true){
+                            cout<<i<<endl;
+                            vec[i+1]=true;
+                            break;
+                        }
+                    }
+                }
+                j--;
+                temp--;
+            }
         }
-        temp.push_back(s[ind]);
-        count++;
-        bool k=helper(s, ind+1, st, n,count, temp, dp);
-        count--;
-        if(k){
-            dp[ind][count]=1;
-            return true;
-        }
-        temp.pop_back();
-        if(st.count(temp)){
-            temp.clear();
-            bool k2=helper(s, ind+1, st, n, 1, temp+s[ind], dp);
-        if(k2){
-            dp[ind][count]=1;
-            return true;
-        }
-        }
-        dp[ind][count]=0;
-        return false;
+        return vec[n];
     }
 };
