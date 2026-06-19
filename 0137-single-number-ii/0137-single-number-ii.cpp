c2764a0876e4bce967b086a2552d188b6e3292c5
SukhamGeek <sukhamsingh3105@gmail.com>
2026-06-19 23:23:54 +0530

    Time: 0 ms (100%), Space: 13.2 MB (99.06%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,12 +3,11 @@ public:
     int singleNumber(vector<int>& nums) {
         int sum=0;
         for(int i=0; i<32; i++){
-            int k= 1<<i;
             int count=0;
             for(int j=0; j<nums.size(); j++){
-                if(nums[j]&k) count++;
+                if(nums[j]&1<<i) count++;
             }
-            if(count%3==1) sum+= k;
+            if(count%3==1) sum+= 1<<i;
         }     
         return sum;  
     }
