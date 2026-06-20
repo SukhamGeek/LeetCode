0a7f6e801a5406ab5b0a29431d64258a61a93390
SukhamGeek <sukhamsingh3105@gmail.com>
2026-06-21 02:07:31 +0530

    Time: 3 ms (27.68%), Space: 16.7 MB (69.87%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>>q;
+        int count=0;
+        int m= grid.size();
+        int n=grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1) count++;
+            }
+        }
+        if(count==0) return 0;
+        int flag=0;
+        while(!q.empty()){
+            int s= q.size();
+            for(int i=0; i<s; i++){
+                auto[a,b]= q.front();
+                q.pop();
+                if((a+1)<m&& grid[a+1][b]!=0 &&grid[a+1][b]!=2){
+                    grid[a+1][b]=2;
+                    q.push({a+1,b});
+                    count--;
+                } 
+                if((b+1)<n &&grid[a][b+1]!=0 &&grid[a][b+1]!=2){
+                    grid[a][b+1]=2;
+                    q.push({a,b+1});
+                    count--;
+                } 
+                if((a-1)>=0 &&grid[a-1][b]!=0 &&grid[a-1][b]!=2){
+                    grid[a-1][b]=2;
+                   q.push({a-1,b});
+                   count--;
+                }
+                if((b-1)>=0 &&grid[a][b-1]!=0 &&grid[a][b-1]!=2){
+                    grid[a][b-1]=2;
+                    q.push({a,b-1});
+                    count--;
+                } 
+            }
+            flag++;
+        }
+        if(count!=0) return -1;
+        return flag-1;
+    }
+};
