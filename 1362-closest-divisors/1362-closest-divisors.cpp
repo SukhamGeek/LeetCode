6522151bc22808c8cdeee0d9c5d6f08a89b3e1df
SukhamGeek <sukhamsingh3105@gmail.com>
2026-06-20 23:22:48 +0530

    Time: 7 ms (42.53%), Space: 8.1 MB (44.8%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -5,23 +5,25 @@ public:
         int b= num+2;
         int mini=INT_MAX;
         vector<int>vec(2);
-        for(int i=1; i<=sqrt(a); i++){
+        for(int i=sqrt(a); i>0; i--){
             if(a%i==0){
                 int k= a/i;
                 if(abs(i-k)<mini){
                     mini= abs(i-k);
                     vec[0]=i;
                     vec[1]=k;
+                    break;
                 } 
             }
         }
-        for(int i=1; i<=sqrt(b); i++){
+        for(int i=sqrt(b); i>0; i--){
             if(b%i==0){
                 int k= b/i;
                 if(abs(i-k)<mini){
                     mini= abs(i-k);
                     vec[0]=i;
                     vec[1]=k;
+                    break;
                 } 
             }
         }   
