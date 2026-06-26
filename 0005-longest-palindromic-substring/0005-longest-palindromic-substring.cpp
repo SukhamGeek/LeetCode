3158ffdb10eb70502fdb2c9fd7404466b6dcf74d
SukhamGeek <sukhamsingh3105@gmail.com>
2026-06-26 23:09:47 +0530

    Time: 8 ms (72.55%), Space: 12 MB (51.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,25 +4,30 @@ public:
         int n= s.size();
         string ans="";
         ans+=s[0];
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                bool b=false;
-                int m=i; 
-                int n=j;
-                while(m<n){
-                    if(s[m]!= s[n]){
-                        b=true;
-                        break;
-                    }
-                    m++;
-                    n--;
+        for(int i=1; i<n; i++){
+            int st=i-1;
+            int e=i+1;
+            while(st>=0 && e<n){
+                if(s[st]!=s[e]){
+                    break;
                 }
-                if(b) continue;
-                else{
-                    if((j-i+1)>ans.size()){
-                        ans= s.substr(i, j-i+1);
-                    }
+                st--;
+                e++;
+            }
+            if((e-st-1)>ans.size()){
+                ans= s.substr(st+1,e-st-1);
+            }
+            st=i-1;
+            e=i;
+            while(st>=0 && e<n){
+                if(s[st]!=s[e]){
+                    break;
                 }
+                st--;
+                e++;
+            }
+            if((e-st-1)>ans.size()){
+                ans= s.substr(st+1,e-st-1);
             }
         }
         return ans;
