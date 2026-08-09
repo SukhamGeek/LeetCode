8ccc5e10cf30eed45d922b3f2fd661ad3be76082
SukhamGeek <sukhamsingh3105@gmail.com>
2026-08-09 13:23:58 +0530

    Time: 8 ms (34.19%), Space: 20.1 MB (24.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -4,29 +4,24 @@ public:
         vector<vector<int>> adj(numCourses);
         queue<int>q;
         vector<int> ind(numCourses,0);
-        vector<int>res;
+        int count=0;
         for(auto it: prerequisites){
             adj[it[0]].push_back(it[1]);
             ind[it[1]]++;
         }
-        bool b= false;
         for(int i=0; i<ind.size(); i++){
-            if(ind[i]==0){
-                q.push(i);
-                b=true;
-            } 
+            if(ind[i]==0) q.push(i);
         }
-        if(!b) return false;
         while(!q.empty()){
             int k= q.front();
-            res.push_back(k);
+            count++;
             q.pop();
             for(auto it: adj[k]){
                 ind[it]--;
                 if(ind[it]==0) q.push(it);
             }
         }
-        if(res.size()==numCourses) return true;
+        if(count==numCourses) return true;
         return false;
     }
 };
