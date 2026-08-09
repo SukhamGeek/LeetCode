47739f778a4edd20b3ff6027e4136bc9cbe64042
SukhamGeek <sukhamsingh3105@gmail.com>
2026-08-09 13:22:03 +0530

    Time: 8 ms (34.19%), Space: 20.3 MB (16.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>arr(numCourses);
-        for(int i=0; i<prerequisites.size(); i++){
-            arr[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        vector<vector<int>> adj(numCourses);
+        queue<int>q;
+        vector<int> ind(numCourses,0);
+        vector<int>res;
+        for(auto it: prerequisites){
+            adj[it[0]].push_back(it[1]);
+            ind[it[1]]++;
         }
-        vector<int>visited(numCourses,0);
-        vector<int>pathvis(numCourses,0);
-        for(int i=0; i<arr.size(); i++){
-            if(visited[i]==0&& arr[i].size()!=0){
-                bool b= helper(i, visited, arr, pathvis);
-                if(b== false) return false;
+        bool b= false;
+        for(int i=0; i<ind.size(); i++){
+            if(ind[i]==0){
+                q.push(i);
+                b=true;
+            } 
+        }
+        if(!b) return false;
+        while(!q.empty()){
+            int k= q.front();
+            res.push_back(k);
+            q.pop();
+            for(auto it: adj[k]){
+                ind[it]--;
+                if(ind[it]==0) q.push(it);
             }
         }
-        return true;
-    }
-    bool helper(int node, vector<int>& visited, vector<vector<int>>&arr, vector<int>& pathvis){
-        if(visited[node]==1&& pathvis[node]==1) return false;
-        else if(visited[node]==1) return true;
-        visited[node]=1;
-        pathvis[node]=1;
-        for(auto it: arr[node]){
-            bool b=helper(it, visited, arr, pathvis);
-            if(!b) return false;
-        }
-        pathvis[node]=0;
-        return true;
+        if(res.size()==numCourses) return true;
+        return false;
     }
 };
