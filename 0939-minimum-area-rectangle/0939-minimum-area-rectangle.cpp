88641683d23b59edfe3b410ae144dd5048b384e9
SukhamGeek <sukhamsingh3105@gmail.com>
2026-08-13 23:41:21 +0530

    Time: 1258 ms (21.27%), Space: 25.6 MB (55.56%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
-        if(points.size()<4) return 0;
-        int mini= INT_MAX;
+        int n = points.size();
+        int mini = INT_MAX;
+
         unordered_set<string> st;
         for (auto &p : points) {
             string key = to_string(p[0]) + "," + to_string(p[1]);
             st.insert(key);
         }
-        unordered_map<int, vector<int>> xco;
-        unordered_map<int, vector<int>> yco;
-        for(int i=0; i< points.size(); i++){
-            int x= points[i][0];
-            int y= points[i][1];
-            if(xco.find(x)==xco.end() || yco.find(y)==yco.end()){
-                xco[x].push_back(y);
-                yco[y].push_back(x); 
-                continue;
-            }
-            else{
-                for(auto a: xco[x]){
-                    for(auto b: yco[y]){
-                        string temp= to_string(b)+ "," + to_string(a);
-                        if(st.count(temp)){
-                            int area= abs(x-b)* abs(y-a);
-                            mini= min(mini, area);
-                        }
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                if (x1 == x2 || y1 == y2)
+                    continue;
+
+                string p1 = to_string(x1) + "," + to_string(y2);
+                string p2 = to_string(x2) + "," + to_string(y1);
+
+                if (st.count(p1) && st.count(p2)) {
+                    int area = abs(x1 - x2) * abs(y1 - y2);
+
+                    mini = min(mini, area);
                 }
-                xco[x].push_back(y);
-                yco[y].push_back(x); 
             }
         }
-        if(mini== INT_MAX) return 0;
-        return mini;
+
+        return mini == INT_MAX ? 0 : mini;
     }
 };
