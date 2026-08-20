09e679822bf14d7d9c3e0fcc89b79ad41003cf17
SukhamGeek <sukhamsingh3105@gmail.com>
2026-08-20 20:20:31 +0530

    Time: 692 ms (24.81%), Space: 151.1 MB (23.71%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,35 @@
+struct pair_hash {
+    size_t operator()(const pair<int,int>& p) const {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n= points.size();
+        int ans=0;
+        unordered_map<pair<int,int>, vector<pair<int,int>>, pair_hash> mpp;
+        for(int i=0; i<n; i++){
+            for(int j= i+1; j<n; j++){
+                mpp[{points[i][0],points[i][1]}].push_back({points[j][0],points[j][1]});
+                mpp[{points[j][0],points[j][1]}].push_back({points[i][0],points[i][1]});
+            }
+        }
+        unordered_map<pair<int,int>, int, pair_hash> vis;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0,{points[0][0],points[0][1]}});
+        while(!pq.empty()){
+            auto[dis, coor]= pq.top();
+            pq.pop();
+            if(vis.find(coor)!=vis.end())continue;
+            ans+= dis;
+            vis[coor]=1;
+            for(auto it: mpp[coor]){
+                if(vis.find(it)!=vis.end()) continue;
+                int dist= abs(coor.first- it.first)+ abs(coor.second - it.second);
+                pq.push({dist,it});
+            }
+        }
+        return ans;
+    }
+};
